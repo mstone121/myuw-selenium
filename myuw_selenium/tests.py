52bfad1bf683a1386031f7e6823f2332f412698a
mstone121 <mstone121@gmail.com>
2015-02-18 12:26:14 -0800

    Added Selenium textbook card landing page tests

--- myuw_selenium/tests.py
+++ myuw_selenium/tests.py
@@ -1,3 +1,9 @@
 # Needed to do both lines to get the dynamically made classes
 from myuw_selenium.test.card_order import CardOrderTest
 from myuw_selenium.test.card_order import *
+
+from myuw_selenium.test.textbook_card_lp import *
+from myuw_selenium.test.textbook_card_lp_mu_2466 import *
+
+
+
