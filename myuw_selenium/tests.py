a701ac107f2a0e7f5145a34ffc2a5f30a7c3c9de
mstone121 <mstone121@gmail.com>
2015-04-23 14:20:54 -0700

    Edited tests and made tests not use xvfb

--- myuw_selenium/tests.py
+++ myuw_selenium/tests.py
@@ -1,15 +1,16 @@
-from myuw_selenium.test.card_order import CardOrderTest
-from myuw_selenium.test.card_order import *
-from myuw_selenium.test.muwm_testing import *
-from myuw_selenium.test.muwm_acal import *
+#from myuw_selenium.test.card_order import CardOrderTest
+#from myuw_selenium.test.card_order import *
+#from myuw_selenium.test.muwm_testing import *
+#from myuw_selenium.test.muwm_acal import *
 
 # Needed to do both lines to get the dynamically made classes
-from myuw_selenium.test.card_order import CardOrderTest
-from myuw_selenium.test.card_order import *
+#from myuw_selenium.test.card_order import CardOrderTest
+#from myuw_selenium.test.card_order import *
 
-from myuw_selenium.test.textbook_card_lp import *
-from myuw_selenium.test.textbook_card_lp_mu_2466 import *
+#from myuw_selenium.test.textbook_card_lp import *
+#from myuw_selenium.test.textbook_card_lp_mu_2466 import *
 
-from myuw_selenium.test.relevant_events import *
+#from myuw_selenium.test.relevant_events import *
 
-from myuw_selenium.test.lib_subject_guides import *
+#from myuw_selenium.test.lib_subject_guides import *
+from myuw_selenium.test.course_eval import *
