f009e01788aa4d0d0574ac36fca630ef033e5091
Matt Stone <mstone121@gmail.com>
2015-06-02 20:36:02 -0700

    Updated course_eval tests.

--- myuw_selenium/test/course_eval.py
+++ myuw_selenium/test/course_eval.py
@@ -6,9 +6,80 @@ from myuw_selenium.test.card_tests_c import create_test_from_test
 
 import datetime
 
+temp_name = "BILL AVERAGE TEACHER"
+#temp_name = "Nancy O\'Brien-Abël"
+
 
 tests = [
     # Javerage
+    # { 'user':'javerage',
+    #   'date':'2013-06-10', 
+    #   'test_name':'Javerage: Eval shown between a week before finals week and the end of finals week', 
+    #   'test':EvalsShownTest,    
+    #   'courses':['TRAIN 100 A', 'TRAIN 101 A']
+    # },
+
+    # { 'user':'javerage', 
+    #   'date':'2013-06-02',
+    #   'test_name':'Javerage: No eval shown a before a week before finals week',
+    #   'test':EvalsNotShownTest,
+    #   'courses':['TRAIN 100 A', 'TRAIN 101 A']
+    # },
+
+    # { 'user':'javerage', 
+    #   'date':'2013-06-15',
+    #   'test_name':'Javerage: No eval shown after finals week',
+    #   'test':EvalsNotShownTest,
+    #   'courses':['TRAIN 100 A', 'TRAIN 101 A']
+    # },
+    
+
+    # { 'user':'javerage',
+    #   'date':'2013-06-10',
+    #   'test_name':'Javerage: Links are correct (display text and url)',
+    #   'test':LinksTest,
+    #   'courses':['TRAIN 100 A', 'TRAIN 101 A'],
+    #   'links':{
+    #       'TRAIN 100 A' : {
+    #           'Rate JAMES AVERAGE STUDENT':'https://uw.iasysdev.org/survey/136617',
+    #       },
+    #       'TRAIN 101 A' : {
+    #           'Rate EIGHT CLASS STUDENT':'https://uw.iasysdev.org/survey/136617',
+    #           ('Rate ' + temp_name):'https://uw.iasysdev.org/survey/1337',
+    #       }
+    #   }
+    # },           
+
+    # { 'user':'javerage',
+    #   'date':'2013-06-10',
+    #   'test_name':'Javerage: Close date is correct', 
+    #   'test':CloseDateTest,    
+    #   'courses':['TRAIN 100 A', 'TRAIN 101 A'],
+    #   'dates': {
+    #       'TRAIN 100 A': [datetime.date(2013, 4, 5), datetime.date(2013, 5, 13)],
+    #       'TRAIN 101 A': [datetime.date(2013, 4, 5), datetime.date(2013, 5, 13)]
+    #   }
+    # },
+
+    # { 'user':'javerage',
+    #   'date':'2013-06-10',
+    #   'test_name':'Javerage: Instructor name is correct',
+    #   'test':InstructorNameTest,
+    #   'courses':['TRAIN 100 A', 'TRAIN 101 A'],
+    #   'names': {
+    #       'TRAIN 100 A': ['JAMES AVERAGE STUDENT'],
+    #       'TRAIN 101 A': ['EIGHT CLASS STUDENT', temp_name]
+    #   }
+    # },
+
+    # { 'user':'javerage',
+    #   'date':'2013-06-10',
+    #   'test_name':'Javerage: Tab access test',
+    #   'test':TabAccessTest,
+    #   'courses':['TRAIN 101 A', 'TRAIN 100 A']
+    # },
+
+    # Winter Quarter
     { 'user':'javerage',
       'date':'2013-06-10', 
       'test_name':'Javerage: Eval shown between a week before finals week and the end of finals week', 
@@ -42,7 +113,7 @@ tests = [
           },
           'TRAIN 101 A' : {
               'Rate EIGHT CLASS STUDENT':'https://uw.iasysdev.org/survey/136617',
-              'Rate Nancy O\'Brien-Abël':'https://uw.iasysdev.org/survey/1337',
+              ('Rate ' + temp_name):'https://uw.iasysdev.org/survey/1337',
           }
       }
     },           
@@ -65,7 +136,7 @@ tests = [
       'courses':['TRAIN 100 A', 'TRAIN 101 A'],
       'names': {
           'TRAIN 100 A': ['JAMES AVERAGE STUDENT'],
-          'TRAIN 101 A': ['EIGHT CLASS STUDENT', 'Nancy O\'Brien-Abël']
+          'TRAIN 101 A': ['EIGHT CLASS STUDENT', temp_name]
       }
     },
 
@@ -76,27 +147,28 @@ tests = [
       'courses':['TRAIN 101 A', 'TRAIN 100 A']
     },
 
+
     # Summer Quarters
     # A-term
     { 'user':'javerage',
       'date':'2013-07-24', 
       'test_name':'Javerage (Summer A-term): Eval shown between a week before last day of Summer A-term', 
       'test':EvalsShownTest,    
-      'courses':['ELCBUS 451 A', 'TRAIN 102 A']
+      'courses':['ELCBUS 451 A']
     },
 
     { 'user':'javerage', 
       'date':'2013-07-17',
       'test_name':'Javerage (Summer A-term): No eval shown a before a week before last day of Summer A-term',
       'test':EvalsNotShownTest,
-      'courses':['ELCBUS 451 A', 'TRAIN 102 A']
+      'courses':['ELCBUS 451 A']
     },
 
     { 'user':'javerage', 
       'date':'2013-07-25',
       'test_name':'Javerage (Summer A-term): No eval shown last day of Summer A-term',
       'test':EvalsNotShownTest,
-      'courses':['ELCBUS 451 A', 'TRAIN 102 A']
+      'courses':['ELCBUS 451 A',]
     },
     
 
@@ -104,11 +176,8 @@ tests = [
       'date':'2013-07-24',
       'test_name':'Javerage (Summer A-term): Links are correct (display text and url)',
       'test':LinksTest,
-      'courses':['TRAIN 102 A', 'ELCBUS 451 A'],
+      'courses':['ELCBUS 451 A'],
       'links':{
-          'TRAIN 102 A' : {
-              'Rate EIGHT CLASS STUDENT':'https://uw.iasystem.org/survey/130810',
-          },
           'ELCBUS 451 A' : {
               'Rate JAMES AVERAGE STUDENT':'https://uw.iasystem.org/survey/130808',
           }
@@ -119,10 +188,9 @@ tests = [
       'date':'2013-07-24',
       'test_name':'Javerage (Summer A-term): Close date is correct', 
       'test':CloseDateTest,    
-      'courses':['TRAIN 102 A', 'ELCBUS 451 A'],
+      'courses':['ELCBUS 451 A'],
       'dates': {
-          'TRAIN 102 A':  [datetime.date(2013, 7, 24), datetime.date(2013, 7, 29)],
-          'ELCBUS 451 A': [datetime.date(2013, 8, 23), datetime.date(2013, 8, 29)]
+          'ELCBUS 451 A': [datetime.date(2013, 7, 24), datetime.date(2013, 7, 29)]
       }
     },
 
@@ -130,9 +198,8 @@ tests = [
       'date':'2013-07-24',
       'test_name':'Javerage (Summer A-term): Instructor name is correct',
       'test':InstructorNameTest,
-      'courses':['TRAIN 102 A', 'ELCBUS 451 A'],
+      'courses':['ELCBUS 451 A'],
       'names': {
-          'TRAIN 102 A': ['EIGHT CLASS STUDENT'],
           'ELCBUS 451 A': ['JAMES AVERAGE STUDENT']
       }
     },
@@ -141,7 +208,7 @@ tests = [
       'date':'2013-07-24',
       'test_name':'Javerage (Summer A-term): Tab access test',
       'test':TabAccessTest,
-      'courses':['ELCBUS 451 A', 'TRAIN 102 A']
+      'courses':['ELCBUS 451 A']
     },
 
     # B-term
