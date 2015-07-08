a01669278f492bcea97684d2678ac5d03105a9ba
Matt Stone <mstone121@gmail.com>
2015-07-07 17:40:26 -0700

    Added muwm-3068 tests

--- myuw_selenium/test/card_tests_c.py
+++ myuw_selenium/test/card_tests_c.py
@@ -5,6 +5,7 @@ from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 import time
 
